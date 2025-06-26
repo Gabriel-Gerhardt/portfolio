be91948c3c785900c3645da5ba492f4c8f86c9dc
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-06-25 21:43:03 -0300

    .

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,10 +3,14 @@ import App from "./App";
 import Any from "./pages/Any";
 import Home from "./pages/Home";
 
+function ErrorPage() {
+  return <div>Ops, página não encontrada!</div>;
+}
 const router = createHashRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "", element: <Home /> },
       { path: "any", element: <Any /> },
